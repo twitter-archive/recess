71d5b622fd3576171b3aa6a7d904b6ec6a78d99c
Gavin Cooper (Home) <gavinsworld@googlemail.com>
2012-06-23 18:26:53 +0100

    added support to inline images in output using option --inlineImages

--- test/types/compile.js
+++ test/types/compile.js
@@ -4,6 +4,10 @@ var fs = require('fs')
   , RECESS = require('../../lib')
 
 fs.readdirSync('test/fixtures').forEach(function (file, index) {
+  // Ignore anything not a less/css file.
+  if (file.indexOf('css') === -1 && file.indexOf('less') === -1) {
+  	return
+  }
 
   RECESS('test/fixtures/' + file, { compile: true  }, function (err, fat) {
     file = file.replace(/less$/, 'css')
